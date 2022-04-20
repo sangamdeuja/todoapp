6c710752a8d3dba3f6c4db303728e4f57850e314
sangamdeuja <sangamad1@gmail.com>
2022-04-20 21:51:49 +0300

    basic client and server code added

---
+++ server/db.js
@@ -0,0 +1,15 @@
+import pg from "pg";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new pg.Pool({
+    user:process.env.RDS_USER,
+    password:process.env.RDS_PASSWORD,
+    host:process.env.RDS_HOST,
+    port:process.env.RDS_PORT,
+    database:process.env.RDS_DATABASE
+
+});
+
+export default pool;
